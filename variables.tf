dcaefe8c2b06c3f5be0b0b8dfebf75c89100667a
tomma <tomma8175@gmail.com>
2022-08-19 11:16:01 +0400

    vars

---
+++ variables.tf
@@ -0,0 +1,49 @@
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-east-1"
+}
+
+variable "aws_availability_zone" {
+  description = "AWS availabitiy zone to launch servers."
+  default     = "us-east-1a"
+}
+
+variable "aws_instance_type" {
+  description = "AWS Instance type"
+  default     = "t2.micro"
+}
+
+
+variable "aws_public_key_name" {
+  default = "prometheus_aws_rsa"
+}
+
+variable "prometheus_access_name" {
+ default = "prometheus_ec2_access"
+}
+
+variable "aws_amis" {
+  default = {
+    us-east-1 = "ami-052efd3df9dad4825"
+  }
+}
+
+variable "name" {
+  description = "Infrastructure name"
+  default = "Promethus_Server"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  default = "11.0.0.0/16"
+}
+
+variable "prometheus_server_subnet_cidr1" {
+  description = "Promethus Server Subnet CIDR"
+  default = "11.0.1.0/24"
+}
+
+variable "env" {
+  description = "Environment"
+  default = "Prod"
+}
